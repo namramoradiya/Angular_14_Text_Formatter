4f2d17f2b50150b7584ad27f553beb30f107f685
namramoradiya <moradiyanamra@gmail.com>
2026-01-15 12:44:06 +0530

    Complete Text Formatter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { FormattersComponent } from './formatters/formatters.component';
+import { TextDisplayComponent } from './text-display/text-display.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
+  // imports: [HomeComponent,TextDisplayComponent,FormattersComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
