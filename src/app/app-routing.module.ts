6d10a50644a782332ed4eb5fc793d8c42eaa71a2
namramoradiya <moradiyanamra@gmail.com>
2026-01-21 17:57:40 +0530

    Added the API Route with Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { APIsComponent } from './apis/apis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'API',component:APIsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
